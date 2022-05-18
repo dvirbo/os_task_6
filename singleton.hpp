a6d364e94c63331a7606e7a4575bf72876de2af0
Dvir Borochov <dvir.borochov@gmail.com>
2022-05-18 12:08:47 +0300

    cont singleton

--- singleton.hpp
+++ singleton.hpp
@@ -5,25 +5,22 @@
 #include <mutex>
 #include <thread>
 
-
 using std::string;
 using std::vector;
 
 class LoadBalancer
 {
-public:
-    static LoadBalancer *GetLoadBalancer();
-    string getServer();
-
 private:
-   static std::mutex mutex_;
+    static LoadBalancer *single;
     LoadBalancer();
-    LoadBalancer(const LoadBalancer &);
-    LoadBalancer &operator=(const LoadBalancer &);
+    vector<string> text;
+    static std::mutex mutex_;
 
-    static LoadBalancer *instance;
-    vector<string> servers;
+public:
+    static LoadBalancer *Instance(); // return pointer to singlton
+    void Destroy();
+    string getServer();
+    
 };
-
 
 // https://gist.github.com/firatakandere/8a19678bf4a36bc6d3a9#file-loadbalancer-h
